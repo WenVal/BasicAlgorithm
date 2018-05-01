8b952785487b1b218c19c82126de66641d109e2a
Val_Wen <38552157+WenVal@users.noreply.github.com>
2018-05-01 18:41:45 +0800

    Add files via upload
    并归排序

---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            MergeSort.sort(arr);
+            foreach (var i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
